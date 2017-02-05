5db0a0eab97d0e78f1af11264e27a4b68289a36b
Chainatee Tanakulrungson <c.tanakul@gmail.com>
2017-02-05 00:19:25 -0600

    can launch with Sawyer model

---
+++ src/SO3_to_pose.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from geometry_msgs.msg import (
+    Pose
+)
+from sawyer_catching_ball.srv import (
+    SO3ToPose
+)
+
+""" service for SO3 to geometry_msgs/Pose"""
+
+# from Jarvis Schultz's quarternion calculation
+def so3_to_axis_angle(R):
+    th = np.arccos((R.trace() - 1)/2)
+    if (th <= 1e-12):
+        th = 0.0
+        w = 3*[1/np.sqrt(3)]
+    else:
+        w = 1/(2*np.sin(th))*np.array([R[2,1]-R[1,2], R[0,2]-R[2,0], R[1,0]-R[0,1]])
+        if any(map(np.isinf, w)) or any(map(np.isnan, w)):
+            th = 0.0
+            w = 3*[1/np.sqrt(3)]
+    return w, th
+
+def axis_angle_to_quat(w, th):
+    return np.array(np.hstack((np.cos(th/2.0), np.array(w)*np.sin(th/2.0))))
+
+def so3_to_quat(R):
+    w,th = so3_to_axis_angle(R)
+    return axis_angle_to_quat(w, th)
+
+def SO3_to_pose_calc(req):
+
+    # SO3 = req.SO3.reshape(4, 4)
+    
+    #reshape 1*16 tuple to 4*4 array
+    SO3_rs = np.zeros((4,4), dtype=np.float64)
+    count = 0
+    for i in range(4):
+        for j in range(4):
+            SO3_rs[i][j] = req.SO3[count]
+            count+=1
+
+    #extract the rotational matrix portion out of SE3
+    R = np.zeros((3,3),dtype=np.float64)
+    for row in range(3):
+        for column in range(3):
+            R[row][column] = SO3_rs[row][column]
+
+    pose = Pose()
+
+
+    #get quat and put in Pose
+    quat = so3_to_quat(R)
+    pose.orientation.w = quat[0] 
+    pose.orientation.x = quat[1]
+    pose.orientation.y = quat[2]
+    pose.orientation.z = quat[3]
+
+    #put position on Pose
+    pose.position.x = SO3_rs[0][3]
+    pose.position.y = SO3_rs[1][3]
+    pose.position.z = SO3_rs[2][3]
+
+    return pose
+
+
+def SO3_to_pose_server():
+    rospy.init_node('SO3_to_pose_server')
+    s = rospy.Service('SO3_to_pose', SO3ToPose, SO3_to_pose_calc)
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    SO3_to_pose_server()
---
+++ src/sawyer_sim.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+import rospy
+from intera_core_msgs.msg import (
+    JointCommand,
+    EndpointState
+)
+from geometry_msgs.msg import (
+    Pose,
+    Wrench,
+    Twist
+)
+from std_msgs.msg import (
+    Header
+)
+from sensor_msgs.msg import (
+    JointState
+)
+from sawyer_catching_ball.srv import (
+    FwdKin
+)
+
+
+def js_eps_init(pres_joint_state, pres_endpoint, request_pos):
+    """
+    a list of joints position for each named position
+    """
+
+    #joint state initialization
+    req_list = ['home']
+    joint_sources = {'home': [0., 0., 0., 0., 0., 0., 0.]}
+    joint_names = ['torso_t0' ,'right_j0', 'right_j1', 'right_j2', 'right_j3','right_j4', 'right_j5', 'right_j6', 'head_camera']
+    pres_joint_state.name = joint_names
+    pres_joint_state.position = range(9)
+    for i in req_list:
+        if request_pos == i: 
+            for j in range(9):
+                if (j == 0 or j == 8):
+                    pres_joint_state.position[j] = 0.
+                else:
+                    pres_joint_state.position[j] = (joint_sources[i])[j - 1]
+    
+
+    #endpoint state initialization
+    
+    # print "1"
+    #ask for fwd kinematics service (input : thetalist, output : pose)
+    rospy.wait_for_service('fwd_kin')
+    # print "2"
+    try:
+
+        fwd_kin = rospy.ServiceProxy('fwd_kin', FwdKin)
+        # print pres_joint_state.position
+        print pres_endpoint.pose
+        pres_endpoint.pose = fwd_kin(pres_joint_state.position).pose #always extract the output out of response
+        print pres_endpoint.pose
+        return pres_joint_state, pres_endpoint
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+
+
+
+
+def velocity_control_server(joints_vel_command, pres_joint_state):
+    """
+    change the currect joint position based on the velocity command with respect to time_step
+    We care only joint0 to joint 6.
+    """
+    #time step in sec
+    dt = 0.01
+    for i in range(9):        
+        if (i == 0 or i == 8):
+            pres_joint_state.position[i] = 0.
+        else:
+            pres_joint_state.position[i] += joints_vel_command.velocity[i] * dt
+            
+
+
+
+def main():
+    rospy.init_node("sawyer_sim")
+    pres_joint_state = JointState() 
+    pres_endpoint = EndpointState()
+    pres_joint_state, pres_endpoint = js_eps_init(pres_joint_state, pres_endpoint,'home')    
+
+    #print pres_joint_state, pres_endpoint
+    #if there's a published msg of velocity controller then moveit
+    rospy.Subscriber("/robot/limb/right/joint_command", JointCommand, velocity_control_server, pres_joint_state)
+
+    pub_ep = rospy.Publisher("/robot/limb/right/endpoint_state", EndpointState, queue_size = 10)
+    pub_js = rospy.Publisher("/robot/joint_states", JointState, queue_size = 10)
+    rate = rospy.Rate(200) #200 Hz
+
+    while not rospy.is_shutdown():
+        #print pres_endpoint
+
+        now = rospy.get_rostime()
+        time_secs = now.secs
+        time_nsecs = now.nsecs
+
+        pres_joint_state.header.stamp.secs = time_secs
+        pres_joint_state.header.stamp.nsecs = time_nsecs
+        pres_endpoint.header.stamp.secs = time_secs
+        pres_endpoint.header.stamp.nsecs = time_nsecs
+
+        pub_js.publish(pres_joint_state)
+        pub_ep.publish(pres_endpoint)
+        rate.sleep()
+        # rospy.spin()
+
+
+if __name__=="__main__":
+    main()
+
